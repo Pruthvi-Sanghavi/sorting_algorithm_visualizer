dacb0561f9fc2e2c8920c2db63a610629fbae023
Pruthvi <45816639+Pruthvi-Sanghavi@users.noreply.github.com>
2021-01-12 18:11:41 -0500

    [SortingVisualizer.jsx] Randomly generates array on click.

---
+++ SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './SortingVisualizer.css';
+
+export default class SortingVisualizer extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			array: [],
+		};
+	}
+	componentDidMount() {
+		this.resetArray();
+	}
+
+	resetArray() {
+		const array = [];
+		for (let i = 0; i < 310; i++) {
+			array.push(randomIntFromIntervals(5,730));
+		}
+		this.setState({array});
+	}
+
+	bubble_sort() {
+
+	}
+
+	render(){
+		const {array} = this.state;
+
+		return (
+			<div className="array-container">
+				{array.map((value, idx) => (
+					<div className="array-bar" key={idx} style={{height: `${value}px`,}}></div>	
+				))}
+				<button onClick={() => this.resetArray()}>Generate New Array</button>
+				<button onClick={() => this.bubble_sort()}>Bubble Sort</button>
+			</div>
+		);
+	}
+}
+
+function randomIntFromIntervals(min, max) {
+	return Math.floor(Math.random()*(max-min+1) + min);
+}
